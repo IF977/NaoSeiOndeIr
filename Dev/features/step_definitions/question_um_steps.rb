79e416495ecc17dbd3c38b13dfdc40a6750ec7ca
JoaoPauloLins <jplo@cin.ufpe.br>
2016-10-22 10:48:50 -0300

    Nova feature #7 Dado que está na pagina form, voltar para home

---
+++ Dev/features/step_definitions/question_um_steps.rb
@@ -0,0 +1,7 @@
+Given(/^I am on the NaoSeiOndeIr form page$/) do
+  visit('/form')
+end
+
+Then(/^I should be on the Home page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
