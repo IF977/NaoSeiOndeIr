a7f89e9df058a5889bf1e7216b0b0eb7f82708d8
JoaoPauloLins <jplo@cin.ufpe.br>
2016-10-26 15:56:48 -0300

    Controller Sugestao #7 Consulta a partir das entradas do usuário na pagina form retorna dados na pagina sugestões

--- app/controllers/sugestao_controller.rb
+++ app/controllers/sugestao_controller.rb
@@ -1,6 +1,10 @@
 class SugestaoController < ApplicationController
 
     def sugestao
+    	puts 'xalala'
+    	a = params[:espirito_local]
+    	b = params[:experiencia]	
+    	@locais = Place.where(["animacao = :el and interacao = :exp", {el: a, exp: b}])
     end
 
 end
