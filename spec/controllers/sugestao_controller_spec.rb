73f900e50b76c1c6b1e4fee9fb1cb6d77de6db91
JoaoPauloLins <jplo@cin.ufpe.br>
2016-11-10 16:23:23 -0300

    Inicio de rspec e gitignore

--- spec/controllers/sugestao_controller_spec.rb
+++ spec/controllers/sugestao_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
+require 'spec_helper'
+require 'place'
 
 RSpec.describe SugestaoController, type: :controller do
-
+	place = Place.new
+	describe "Instance" do
+		it "Instance of local" do
+		   expect(place).to be_an_instance_of(Place)
+		end
+	end
 end
