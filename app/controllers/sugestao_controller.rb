62166bfc1e1af47a3c42fcbdbb9220a7040de0ae
Matheus <mrol@cin.ufpe.br>
2016-11-11 01:55:47 -0300

    alteracao da seed e validacao da insercao pelo scaffold

--- app/controllers/sugestao_controller.rb
+++ app/controllers/sugestao_controller.rb
@@ -3,7 +3,7 @@ class SugestaoController < ApplicationController
     def sugestao
     	a = params[:espirito_local]
     	b = params[:experiencia]	
-    	@locais = Place.where(["animacao = :el and interacao = :exp", {el: a, exp: b}])
+    	@locais = Lugar.where(["animacao = :el and interacao = :exp", {el: a, exp: b}])
     end
 
 end
