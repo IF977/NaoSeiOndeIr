aa5505d5b5775750520fb1431a9a31e21907f9fc
JoaoPauloLins <jplo@cin.ufpe.br>
2016-11-16 15:08:41 -0300

    Filtro do horario refinado Agora o usuário informa a hora que ele deseja chegar no local e o site retorna todos os lugares que estão abertos nessa hora

--- app/controllers/sugestao_controller.rb
+++ app/controllers/sugestao_controller.rb
@@ -5,7 +5,7 @@ class SugestaoController < ApplicationController
     	b = params[:experiencia]
     	c = params[:hora_inicial]
     	d = params[:tipo]
-    	@locais = Local.where(["animacao = :el and interacao = :exp and horario_abre = :h and tipo = :t", {el: a, exp: b, h: c, t: d}])
+    	@locais = Local.where(["animacao = :el and interacao = :exp and :h between (select horario_abre from locals where animacao = :el and interacao = :exp) and (select horario_fecha from locals where animacao = :el and interacao = :exp) and tipo = :t", {el: a, exp: b, h: c, t: d}])
     end
 
 end
