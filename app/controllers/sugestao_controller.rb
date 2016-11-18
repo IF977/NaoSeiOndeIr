79d3ffe7652c7c7d366d5783794235e2613582ca
JoaoPauloLins <jplo@cin.ufpe.br>
2016-11-18 14:06:13 -0300

    Busca melhorada Agara os campos não são mais obrigatórios

--- app/controllers/sugestao_controller.rb
+++ app/controllers/sugestao_controller.rb
@@ -5,7 +5,7 @@ class SugestaoController < ApplicationController
     	b = params[:experiencia]
     	c = params[:hora_inicial]
     	d = params[:tipo]
-    	@locais = Local.where(["animacao = :el and interacao = :exp and (horario_abre<=:h and horario_fecha>:h) and tipo = :t", {el: a, exp: b, h: c, t: d}])
+    	@locais = Local.where(["(animacao = :el or :el is null) and (interacao = :exp or :exp is null) and ((horario_abre<=:h and horario_fecha>:h) or :h = '') and (tipo = :t or :t is null)", {el: a, exp: b, h: c, t: d}])
     end
 
 end
