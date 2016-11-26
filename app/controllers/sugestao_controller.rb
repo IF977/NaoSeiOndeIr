85b744965bbda2f1675c5f9eda96a08911a6efa6
guuhsampaio <gss6@cin.ufpe.br>
2016-11-26 01:05:49 -0300

    Adaptação do front-end

--- app/controllers/sugestao_controller.rb
+++ app/controllers/sugestao_controller.rb
@@ -2,8 +2,10 @@ class SugestaoController < ApplicationController
 
     def sugestao
     	a = params[:espirito_local]
-    	b = params[:experiencia]	
-    	@locais = Place.where(["animacao = :el and interacao = :exp", {el: a, exp: b}])
+    	b = params[:experiencia]
+    	c = params[:hora_inicial]
+    	d = params[:tipo]
+    	@locais = Local.where(["(animacao = :el or :el is null) and (interacao = :exp or :exp is null) and ((horario_abre<=:h and horario_fecha>:h) or :h = '') and (tipo = :t or :t is null)", {el: a, exp: b, h: c, t: d}])
     end
 
 end
