a20e2a5d291806f3aec02784ec63294ae70fe0db
Matheus <mrol@cin.ufpe.br>
2016-11-28 17:44:49 -0300

    ACABOU!!

--- app/controllers/sugestao_controller.rb
+++ app/controllers/sugestao_controller.rb
@@ -5,7 +5,11 @@ class SugestaoController < ApplicationController
     	b = params[:experiencia]
     	c = params[:hora_inicial]
     	d = params[:tipo]
-    	@locais = Local.where(["(animacao = :el or :el is null) and (interacao = :exp or :exp is null) and ((horario_abre<=:h and horario_fecha>:h) or :h = '') and (tipo = :t or :t is null)", {el: a, exp: b, h: c, t: d}])
+      e = params[:valor]
+      if e != nil
+        e = "%"+e+"%"
+      end
+      @locais = Local.where(["(animacao = :el or :el is null) and (interacao = :exp or :exp is null) and ((horario_abre<=:h and horario_fecha>:h) or :h = '' or :h is null) and (tipo = :t or :t is null) and (nome like :v or :v is null)", {el: a, exp: b, h: c, t: d, v: e}])
     end
 
 end
